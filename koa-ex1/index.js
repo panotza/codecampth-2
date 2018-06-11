296176bcd868a5fcec9ffc21355d56f3ba35f41f
panotza <panotza_ja@hotmail.com>
2018-06-11 21:14:02 +0700

    add koa-ex1

---
+++ koa-ex1/index.js
@@ -0,0 +1,37 @@
+const Koa = require('koa')
+const Router = require('koa-router')
+const render = require('koa-ejs')
+const path = require('path')
+const koaBody = require('koa-body')
+
+const app = new Koa()
+const router = new Router()
+render(app, {
+  root: path.join(__dirname, 'views'),
+  layout: 'template',
+  viewExt: 'ejs',
+  cache: false
+})
+
+async function signinGetHandler (ctx) {
+  await ctx.render('signin')
+}
+
+function signinPostHandler (ctx) {
+  console.log('username: ', ctx.request.body.username)
+  console.log('password: ', ctx.request.body.password)
+  ctx.status = 303
+  ctx.redirect('/signin')
+}
+
+router.get('/', ctx => {
+  ctx.body = `<a href="/signin">goto signin page</a>`
+})
+router.get('/signin', signinGetHandler)
+router.post('/signin', signinPostHandler)
+
+app.use(koaBody())
+app.use(router.routes())
+app.use(router.allowedMethods())
+
+app.listen(3000)
